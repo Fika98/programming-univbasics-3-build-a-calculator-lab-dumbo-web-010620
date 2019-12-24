18690b4c05341d4529a180af4f2cae9fbcc8f499
Firuz Afzalov <afzalov98@gmail.com>
2019-12-24 07:44:06 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -10,13 +10,14 @@ subtraction(1,5)
 
 def division(num1, num2)
 divi = num1 / num2
-return divi
+
 end
 division(4,4)
 
 def multiplication(num1, num2)
 multi = num1 * num2
 end
+multiplication(num1,num2)
 
 def modulo(num1, num2)
 
